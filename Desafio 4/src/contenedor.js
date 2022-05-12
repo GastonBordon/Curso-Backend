13f3ea0d31e34a06a4ed65bdcdb752fa49499289
Gaston Bordon <gastonbordon90@gmail.com>
2022-05-11 23:54:52 -0300

    elimino comentarios en contenedor.js

--- Desafio 4/src/contenedor.js
+++ Desafio 4/src/contenedor.js
@@ -1,34 +1,5 @@
 const fs = require("fs");
 const path = "./productos.txt";
-
-const producto1 = {
-  title: "Paleta de Pintor",
-  price: 168,
-  thumbnail:
-    "https://cdn4.iconfinder.com/data/icons/education-759/2050/Education_flat-06-512.png",
-};
-// const producto2 = {
-//   id: 2,
-//   title: "Microscopio",
-//   price: 1250,
-//   thumbnail:
-//     "https://cdn4.iconfinder.com/data/icons/education-759/2050/Education_flat-10-512.png",
-// };
-// const producto3 = {
-//   id: 3,
-//   title: "Lápiz HB",
-//   price: 15,
-//   thumbnail:
-//     "https://cdn4.iconfinder.com/data/icons/to-cool-for-school/512/pencil-512.png",
-// };
-// const producto4 = {
-//   id: 4,
-//   title: "Tijera",
-//   price: 40,
-//   thumbnail:
-//     "https://cdn4.iconfinder.com/data/icons/to-cool-for-school/512/closed-scissor-512.png",
-// };
-
 class ContenedorArchivo {
   constructor(path) {
     this.path = path;
